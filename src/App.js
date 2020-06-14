20563b0bb699ebe84a4f616fb00f97deb8da18d6
sudo <orgpak2011@gmail.com>
2020-06-14 14:32:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <b>Updated By Abdulmajeed</b>
           Learn React
         </a>
       </header>
